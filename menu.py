da927a6ede6939f1b84f78dfd9c1f3c17cf478e4
MonsterLuni <monsterluni.specific@gmail.com>
2023-07-01 15:54:40 +0200

    color now gets shown in RightClickMenu

--- menu.py
+++ menu.py
@@ -4,22 +4,37 @@ collide = []
 number = 0
 id = 0
 points = []
+colors = [[(255,0,0,255),0, "Red"],[(0,255,0,255),0, "Green"],[(0,0,255,255),0, "Blue"],[(255,255,0,255),0, "Yellow"],[(255,0,255,255),0, "Pink"],[(255,255,255,255),0, "White"]]
 
 def rightClickMenu(screen, mouse_pos, amount, new):
     global collide, number, id, points
     if new:
         points = []
-        id = 0
-    while id < amount:
+        id = 1
+    while id <= amount:
         menurect = p.Rect(mouse_pos[0], mouse_pos[1] + (id * 25), 50, 25)
+        match id:
+            case 1:
+                color = colors[0][0]
+            case 2:
+                color = colors[1][0]
+            case 3:
+                color = colors[2][0]
+            case 4:
+                color = colors[3][0]
+            case 5:
+                color = colors[4][0]
+            case 6:
+                color = colors[5][0]
         points.append([
-            id,
-            menurect
+            id - 1,
+            menurect,
+            color
         ])
         id += 1
     for point in points:
         colliderect = point[1].collidepoint(p.mouse.get_pos())
-        colorrect = (255, 0, 0) if colliderect else (255,215,0)
+        colorrect = (80, 80, 80, 20) if colliderect else point[2]
         p.draw.rect(screen, colorrect, point[1])
         if colliderect:
             number = point[0] + 1
